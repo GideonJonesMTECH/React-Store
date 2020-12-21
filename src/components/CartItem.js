e5d866b2bc4eca9e125a44e86cb85e83ac11aa76
Gideon Jones <gideon.jones8310@stu.mtec.edu>
2020-12-21 10:38:52 -0700

    Cart Item Component

---
+++ src/components/CartItem.js
@@ -0,0 +1,35 @@
+import {Component} from 'react';
+
+export class CartItem extends Component{
+ render(){
+  return(
+   <div>
+     <img style={{
+      resizeMode: "cover",
+      height: 200,
+      width: 200
+      }}
+      src={this.props.img}>
+     </img>
+     <div className="CartItemText">
+      <h3 className="CartItemTitle">{this.props.title}</h3> 
+      <h3 className="CartItemPrice">${this.props.price}</h3>
+      <label for="Quantity">Quantity:</label>
+      <select name="Quantity">
+       <option value="1">1</option>
+       <option value="2">2</option>
+       <option value="3">3</option>
+       <option value="4">4</option>
+       <option value="5">5</option>
+       <option value="6">6</option>
+       <option value="7">7</option>
+       <option value="8">8</option>
+       <option value="9">9</option>
+       <option value="10">10</option>
+      </select>
+      <button>Remove</button>
+     </div>
+   </div>
+  );
+ }
+}
